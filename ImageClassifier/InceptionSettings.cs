393e8c0abcd0c70ab46e3e28f8c0ec3c0ef5c454
Martinatjie <martinabrockmann@gmail.com>
2025-04-21 13:54:32 +0200

    Add project files.

---
+++ ImageClassifier/InceptionSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageClassifier
+{
+    struct InceptionSettings
+    {
+        public const int ImageHeight = 224;
+        public const int ImageWidth = 224;
+        public const float Mean = 117;
+        public const float Scale = 1;
+        public const bool ChannelsLast = true;
+    }
+
+}
